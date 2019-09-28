5d01cc02add296eb8e2800144cf8fc78a3bdf10c
Katie Thornhill <katie.thornhill@citrix.com>
2019-09-27 22:00:50 -0400

    remove jest setup stuff from require.context attempts

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'enzyme',
   setupFilesAfterEnv: ['jest-enzyme'],
-  setupFiles: ['<rootDir>/hack/jest.setup.js'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
   globals: {
     'ts-jest': {
