f7cf95cbb06987f866fcfc0af4da1eccdaeffa88
Katie Thornhill <katie.thornhill@citrix.com>
2019-09-29 23:33:58 -0400

    chore: cleanup unused imports

--- atoms/breakpoints/Context.ts
+++ atoms/breakpoints/Context.ts
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { object, ValidationMap } from 'prop-types';
 import { withContext } from 'utils/withContext';
 
 export type ScreenSize = {
--- atoms/colors/colors.tsx
+++ atoms/colors/colors.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { css, createGlobalStyle } from 'styled-components';
 import chroma from 'chroma-js';
 
