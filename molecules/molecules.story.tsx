e194e2be118cf660500f40f9e5b04cf07fbab113
Kata <git@kat.cafe>
2019-12-31 15:22:17 -0500

    chore(storybooks): remove molecule placeholder

--- molecules/molecules.story.tsx
+++ molecules/molecules.story.tsx
@@ -1,5 +1,2 @@
 import { makeFactory } from '../.storybook/storyHelper';
 export const moleculeStories = makeFactory('Molecules');
-import * as React from 'react';
-
-moleculeStories('Placeholder', module).add('Placeholder', () => <div>nya!</div>);
