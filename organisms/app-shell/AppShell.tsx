2ff7914fc3a177f6b6c0d7a3ddec4609983b1c89
Kata <git@kat.cafe>
2020-02-08 18:53:58 -0500

    feat(AppShell): build out basics of AppShell

--- organisms/app-shell/AppShell.tsx
+++ organisms/app-shell/AppShell.tsx
@@ -2,17 +2,18 @@ import * as React from 'react';
 import * as Masthead from 'organisms/masthead';
 import { RoleypolyUser } from '@roleypoly/rpc/shared';
 import { Footer } from 'molecules/footer';
+import { Content } from './AppShell.styled';
 
 type AppShellProps = {
     children: React.ReactNode;
     user: RoleypolyUser.AsObject | null;
-    showFooter: boolean;
+    showFooter?: boolean;
 };
 
 export const AppShell = (props: AppShellProps) => (
     <>
         {props.user !== null ? <Masthead.Authed user={props.user} /> : <Masthead.Guest />}
-        {props.children}
+        <Content>{props.children}</Content>
         {props.showFooter && <Footer />}
     </>
 );
