07cbba0917adc325c79f0e731a74191f126e51ff
Kata <git@kat.cafe>
2020-02-08 18:23:11 -0500

    feat(Footer): finish up basic footer

--- molecules/footer/Footer.tsx
+++ molecules/footer/Footer.tsx
@@ -1,4 +1,27 @@
 import * as React from 'react';
-import { FooterWrapper } from './Footer.styled';
+import { FooterWrapper, HoverColor } from './Footer.styled';
+import { AmbientLarge } from 'atoms/typography';
+import { FaHeart } from 'react-icons/fa';
+import { Flags } from './Flags';
 
-export const Footer = () => <FooterWrapper>Footer</FooterWrapper>;
+const year = new Date().getFullYear();
+
+export const Footer = () => (
+    <FooterWrapper>
+        <AmbientLarge>
+            <div>
+                &copy; {year} Roleypoly &ndash; Made with{' '}
+                <FaHeart size={'0.8em'} color={'#fe4365'} />
+                &nbsp;in Raleigh, NC
+            </div>
+            <div>
+                <a href="https://discord.gg/Xj6rK3E">Discord/Support</a> &ndash;&nbsp;
+                <a href="https://patreon.com/kata">Patreon</a> &ndash;&nbsp;
+                <a href="https://github.com/roleypoly">GitHub</a>
+            </div>
+            <HoverColor>
+                <Flags height={'1em'} />
+            </HoverColor>
+        </AmbientLarge>
+    </FooterWrapper>
+);
