ca2cd69fbcf11f06a420831ad746a2907e8e8fcf
Kata <git@kat.cafe>
2020-02-08 17:07:38 -0500

    feat: add branding atom

---
+++ atoms/branding/Branding.story.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { atomStories } from 'atoms/atoms.story';
+import { Logomark, Logotype } from './Branding';
+import styled from 'styled-components';
+
+const story = atomStories('Branding', module);
+
+const Wrapper = styled.div`
+  background-color: black;
+  padding: 2em;
+`;
+
+story.add('Logomark', () => (
+  <Wrapper>
+    <Logomark />
+  </Wrapper>
+));
+
+story.add('Logotype', () => (
+  <Wrapper>
+    <Logotype />
+  </Wrapper>
+));
