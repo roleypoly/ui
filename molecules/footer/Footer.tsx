29cb8ef5467510c1b178140188172a5b5f375414
Kata <git@kat.cafe>
2020-02-08 17:09:33 -0500

    feat: add AppShell and Footer placeholder parts

---
+++ molecules/footer/Footer.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { FooterWrapper } from './Footer.styled';
+
+export const Footer = () => <FooterWrapper>Footer</FooterWrapper>;
---
+++ organisms/app-shell/AppShell.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import * as Masthead from 'organisms/masthead';
+import { RoleypolyUser } from '@roleypoly/rpc/shared';
+import { Footer } from 'molecules/footer';
+
+type AppShellProps = {
+  children: React.ReactNode;
+  user: RoleypolyUser.AsObject | null;
+  showFooter: boolean;
+};
+
+export const AppShell = (props: AppShellProps) => (
+  <>
+    {props.user !== null ? <Masthead.Authed user={props.user} /> : <Masthead.Guest />}
+    {props.children}
+    {props.showFooter && <Footer />}
+  </>
+);
