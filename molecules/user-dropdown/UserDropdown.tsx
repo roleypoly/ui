0ba9c228ad8ec05e99908d980bfb1096d1dc2995
Kata <git@kat.cafe>
2020-02-08 17:10:07 -0500

    feat: add placeholder UserDropdown molecule

---
+++ molecules/user-dropdown/UserDropdown.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { RoleypolyUser } from '@roleypoly/rpc/shared';
+
+type Props = {
+  user: RoleypolyUser.AsObject;
+};
+
+export const UserDropdown = (props: Props) => <div></div>;
