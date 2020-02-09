c2a4de9e1a329565065a057b38fb92b8a10974f9
Kata <git@kat.cafe>
2020-02-08 22:15:04 -0500

    chore: remove templates placeholder

--- templates/templates.story.tsx
+++ templates/templates.story.tsx
@@ -1,5 +1,2 @@
 import { makeFactory } from '../.storybook/storyHelper';
 export const templateStories = makeFactory('Templates');
-import * as React from 'react';
-
-templateStories('Placeholder', module).add('Placeholder', () => <div>nya!</div>);
